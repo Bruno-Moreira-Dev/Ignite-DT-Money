98999bf3115f388acc8261ece3cabbd8614a44d4
Bruno-Moreira-Dev <marbruge@gmail.com>
2024-02-06 21:35:59 -0300

    feat(dt-money): utilzacao/tratativa de busca/listagem de transacoes

--- src/contexts/TransactionContext.tsx
+++ src/contexts/TransactionContext.tsx
@@ -1,40 +1,82 @@
-import { ReactNode, createContext, useEffect, useState } from "react";
+import { ReactNode, useCallback, useEffect, useState } from 'react'
+import { createContext } from 'use-context-selector'
+import { api } from '../lib/axios'
 
 interface ITransaction {
-    id: number;
-    description: string;
-    type: 'income' | 'outcome';
-    price: number;
-    category: string;
-    createdAt: string;
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
+
+interface ICreateTransactionInput {
+  description: string
+  price: number
+  category: string
+  type: 'income' | 'outcome'
 }
 
 interface TransactionContextType {
-    transactions: ITransaction[];
+  transactions: ITransaction[]
+  fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (data: ICreateTransactionInput) => Promise<void>
 }
 
-export const TransactionsContext = createContext<TransactionContextType>({} as TransactionContextType);
+export const TransactionsContext = createContext<TransactionContextType>(
+  {} as TransactionContextType,
+)
 
 interface ITransactionProviderProps {
-    children: ReactNode;
+  children: ReactNode
 }
 
 export function TransactionsProvider({ children }: ITransactionProviderProps) {
-    const [transactions, setTransactions] = useState<ITransaction[]>([]);
+  const [transactions, setTransactions] = useState<ITransaction[]>([])
 
-    async function loadTransactions() {
-        const response = await fetch("http://localhost:3000/transactions");
-        const data = await response.json();
-        setTransactions(data);
-    }
+  const fetchTransactions = useCallback(async (query?: string) => {
+    const response = await api.get('transactions', {
+      params: {
+        _sort: 'createdAt',
+        _order: 'desc',
+        q: query,
+      },
+    })
 
-    useEffect(() => {
-        loadTransactions();
-    }, []);
+    setTransactions(response.data)
+  }, [])
 
-    return (
-        <TransactionsContext.Provider value={{ transactions }}>
-            {children}
-        </TransactionsContext.Provider>
-    )
+  const createTransaction = useCallback(
+    async (data: ICreateTransactionInput) => {
+      const { description, price, category, type } = data
+
+      const response = await api.post('transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(),
+      })
+
+      setTransactions((state) => [...state, response.data])
+    },
+    [],
+  )
+
+  useEffect(() => {
+    fetchTransactions()
+  }, [fetchTransactions])
+
+  return (
+    <TransactionsContext.Provider
+      value={{
+        transactions,
+        fetchTransactions,
+        createTransaction,
+      }}
+    >
+      {children}
+    </TransactionsContext.Provider>
+  )
 }
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,40 +1,69 @@
-import { MagnifyingGlass } from "phosphor-react";
-import { SearchFormContainer } from "./styles";
-import { useForm } from "react-hook-form";
-import * as z from 'zod';
-import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { useContextSelector } from 'use-context-selector'
+import * as z from 'zod'
+import { TransactionsContext } from '../../../../contexts/TransactionContext'
+import { SearchFormContainer } from './styles'
+import { memo } from 'react'
+
+/**
+ * Por que um componente renderiza?
+ *
+ * - Hooks changed (mudou estado, contexto, reducer);
+ * - Props changed (mudou propriedade);
+ * - Parent rerendered (componente pai renderizou);
+ *
+ * Qual o fluxo de renderização?
+ * 1. O React recria o HTML da interface daquele componente.
+ * 2. Compara a versão do HTML recriada com a versão anterior.
+ * 3. Se mudou alguma coisa, ele reescreve o HTML na tela.
+ *
+ * Memo:
+ * 0. Hooks changed, Props changed (deep comparison).
+ * 0.1 Comparar a versão anterior dos hooks e props.
+ * 0.2 Se mudou algo, ele vai permitir a nova renderização.
+ */
 
 const searchFormSchema = z.object({
-    query: z.string(),
-});
+  query: z.string(),
+})
 
-type SearchFormInputs = z.infer<typeof searchFormSchema>;
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm() {
-    const {
-        register,
-        handleSubmit,
-        formState: { isSubmitting }
-    } = useForm<SearchFormInputs>({
-        resolver: zodResolver(searchFormSchema),
-    });
+function SearchFormComponent() {
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
-    async function handleSearchTransactions(data: SearchFormInputs) {
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        console.log(data);
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
 
-    return (
-        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
-            <input
-                type="text"
-                placeholder="Busque por transações"
-                {...register('query')}
-            />
-            <button type="submit" disabled={isSubmitting}>
-                <MagnifyingGlass size={20} />
-                Buscar
-            </button>
-        </SearchFormContainer>
-    )
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
 }
+
+export const SearchForm = memo(SearchFormComponent)
